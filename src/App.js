3922baa7662d8f9c050d3a22e7aae7e80fcbca31
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-08-23 13:27:49 +0300

    Used an async function

--- src/App.js
+++ src/App.js
@@ -55,11 +55,17 @@ const KEY = "1483b361";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const query = "Interstellar";
 
   useEffect(function () {
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=Interstellar`).then((res) =>
-      res.json().then((data) => setMovies(data.Search))
-    );
+    async function fetchMovies() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+    }
+    fetchMovies();
   }, []);
 
   return (
